addb4b6b3309869217ac23259e2c295f57a66ad2
Gustavo Ferreira <gdragnell@live.com>
2020-06-30 20:55:31 -0300

    Servico

---
+++ Servico/Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servico.Entities
+{
+    class Client
+    {
+        public string ClientName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client(string clientname, string email, DateTime birthdate)
+        {
+            ClientName = clientname;
+            Email = email;
+            BirthDate = birthdate;
+        }
+
+        public override string ToString()
+        {
+            return ClientName
+                + ", ("
+                + BirthDate.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
+    }
+}
+
---
+++ Servico/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servico.Entities
+{
+    class Product
+    {
+        public String ProdName { get; set; }
+        public double Price { get; set; }
+
+        public Product(string prodname, double price)
+        {
+            ProdName = prodname;
+            Price = price;
+        }
+    }
+}
+
